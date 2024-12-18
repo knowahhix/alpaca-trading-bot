450eec2f4a970a43cf0cb83e70902224746c49e6
Noah Hicks <nhicks@bandwidth.com>
2024-12-18 16:35:30 -0500

    Update buy_script.go

--- alpaca/buy_script.go
+++ alpaca/buy_script.go
@@ -166,9 +166,11 @@ func getStartPrice(symbol string, alpacaKey string, alpacaSecret string) float64
 
 	bars := data["bars"].([]interface{})
 
+	bar := bars[0].(interface{})
+
 	fmt.Print(bars)
 
-	return bars["o"].(float64)
+	return bar["o"].(float64)
 }
 
 // func buyOrder(symbol string, client *alpaca.Client) {

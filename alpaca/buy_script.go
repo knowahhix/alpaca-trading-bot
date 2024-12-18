60fff1e25ced79c157beebbcc5e015f7b453e0f6
Noah Hicks <noahg@ad.unc.edu>
2024-12-17 22:22:16 -0500

    auth

--- alpaca/buy_script.go
+++ alpaca/buy_script.go
@@ -19,8 +19,9 @@ func main () {
 	assetsUrl := "https://api.alpaca.markets/v2/assets"
 
 	formattedString := fmt.Sprintf("%s:%s", username, password)
-	fmt.Printf("%s", formattedString)
+	
 	auth := base64.StdEncoding.EncodeToString([]byte(formattedString))
+	fmt.Printf("%s", auth)
 
 	params := "?status=active&asset_class=us_equity&exchange=NASDAQ"
 

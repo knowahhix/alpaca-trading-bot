37212a84151c5d4005a6ee191c58a8cd9656f8e2
Noah Hicks <noahg@ad.unc.edu>
2024-12-17 22:16:11 -0500

    updates

--- alpaca/buy_script.go
+++ alpaca/buy_script.go
@@ -1,126 +1,148 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
-	"math"
+	"io"
+	"net/http"
 	"os"
-
-	"github.com/alpacahq/alpaca-trade-api-go/alpaca"
-	"github.com/alpacahq/alpaca-trade-api-go/common"
-	"github.com/shopspring/decimal"
+	// "github.com/alpacahq/alpaca-trade-api-go/alpaca"
 )
 type SymbolChange struct {
 	Symbol string
 	Change float32
 }
 
-func main() {
-	alpacaKey, _ := os.LookupEnv("ALPACA_KEY")
-	alpaceSecret, _ := os.LookupEnv("ALPACA_SECRET")
-	fmt.Printf("%s", alpacaKey)
-	baseUrl := "https://paper-api.alpaca.markets"
-
-	alpaca.SetBaseUrl(baseUrl)
-	creds := common.APIKey{
-		ID: alpacaKey,
-		Secret: alpaceSecret,
-	}
-
-	client := alpaca.NewClient(&creds)
-
-	status := "active"
-	assets, err := client.ListAssets(&status)
-	if err != nil {
-		panic(err)
-	}
+func main () {
+	username, _ := os.LookupEnv("ALPACA_USERNAME")
+	password, _ := os.LookupEnv("ALPACA_PASSWORD")
+	assetsUrl := "https://broker-api.sandbox.alpaca.markets/v1/assets"
 	
-	nasdaq_assets := []alpaca.Asset{}
-	for _, asset := range assets {
-		if asset.Exchange == "NASDAQ" {
-			nasdaq_assets = append(nasdaq_assets, asset)
-		}
-	}
+	formattedString := fmt.Sprintf("%s:%s", username, password)
+	auth := base64.StdEncoding.EncodeToString([]byte(formattedString))
 
-	first, second := findBiggestLosers(client, nasdaq_assets)
+	params := "?status=active&asset_class=us_equity&exchange=NASDAQ"
 
-	buyOrder(first.Symbol, client)
-	buyOrder(second.Symbol, client)
+	req, _ := http.NewRequest("GET", assetsUrl + params, nil)
+
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("authorization", fmt.Sprintf("Basic %s", auth))
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	fmt.Println(string(body))
 }
 
-func findBiggestLosers(client *alpaca.Client, assets []alpaca.Asset) (SymbolChange, SymbolChange){
-	losers := []SymbolChange{}
-	for _, asset := range assets {
-		open := getStartPrice(asset.Symbol, client)
-		current := getCurrentPrice(asset.Symbol, client)
-		percentChange := (current / open) * 100
-		data := SymbolChange{
-			Symbol: asset.Symbol,
-			Change: percentChange,
-		}
-		losers = append(losers, data)
-	}
+// func main() {
+// 	alpacaKey, _ := os.LookupEnv("ALPACA_KEY")
+// 	alpaceSecret, _ := os.LookupEnv("ALPACA_SECRET")
+// 	fmt.Printf("%s", alpacaKey)
+// 	baseUrl := "https://paper-api.alpaca.markets"
+
+// 	alpaca.SetBaseUrl(baseUrl)
+// 	creds := common.APIKey{
+// 		ID: alpacaKey,
+// 		Secret: alpaceSecret,
+// 	}
+
+// 	client := alpaca.NewClient(&creds)
+
+// 	status := "active"
+// 	assets, err := client.ListAssets(&status)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 	
-	var first, second SymbolChange
-	first.Change = math.MaxFloat32
+// 	nasdaq_assets := []alpaca.Asset{}
+// 	for _, asset := range assets {
+// 		if asset.Exchange == "NASDAQ" {
+// 			nasdaq_assets = append(nasdaq_assets, asset)
+// 		}
+// 	}
 
-	for _, sc := range losers {
-		if sc.Change < first.Change {
-			// Update first and second smallest values
-			second = first
-			first = sc
-		} else if sc.Change < second.Change {
-			// Update only the second smallest value
-			second = sc
-		}
-	}
+// 	first, second := findBiggestLosers(client, nasdaq_assets)
 
-	return first, second
+// 	buyOrder(first.Symbol, client)
+// 	buyOrder(second.Symbol, client)
+// }
 
-}
+// func findBiggestLosers(client *alpaca.Client, assets []alpaca.Asset) (SymbolChange, SymbolChange){
+// 	losers := []SymbolChange{}
+// 	for _, asset := range assets {
+// 		open := getStartPrice(asset.Symbol, client)
+// 		current := getCurrentPrice(asset.Symbol, client)
+// 		percentChange := (current / open) * 100
+// 		data := SymbolChange{
+// 			Symbol: asset.Symbol,
+// 			Change: percentChange,
+// 		}
+// 		losers = append(losers, data)
+// 	}
+	
+// 	var first, second SymbolChange
+// 	first.Change = math.MaxFloat32
 
-func getCurrentPrice(symbol string, client *alpaca.Client) float32 {
-	resp, err := client.GetLastQuote(symbol)
+// 	for _, sc := range losers {
+// 		if sc.Change < first.Change {
+// 			// Update first and second smallest values
+// 			second = first
+// 			first = sc
+// 		} else if sc.Change < second.Change {
+// 			// Update only the second smallest value
+// 			second = sc
+// 		}
+// 	}
 
-	if err != nil {
-		panic(err)
-	}
+// 	return first, second
 
-	return resp.Last.AskPrice
-}
+// }
 
-func getStartPrice(symbol string, client *alpaca.Client) float32 {
-	limit := 1
-	params := alpaca.ListBarParams{
-		Timeframe: "1D",
-		Limit: &limit,
-	}
+// func getCurrentPrice(symbol string, client *alpaca.Client) float32 {
+// 	resp, err := client.GetLastQuote(symbol)
 
-	bars, err := client.GetSymbolBars(symbol, params)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	if err != nil {
-		panic(err)
-	}
+// 	return resp.Last.AskPrice
+// }
 
-	openPrice := bars[0].Open
+// func getStartPrice(symbol string, client *alpaca.Client) float32 {
+// 	limit := 1
+// 	params := alpaca.ListBarParams{
+// 		Timeframe: "1D",
+// 		Limit: &limit,
+// 	}
 
-	return openPrice
-}
+// 	bars, err := client.GetSymbolBars(symbol, params)
 
-func buyOrder(symbol string, client *alpaca.Client) {
-	accountID, err := client.GetAccount()
-	if err != nil {
-		panic(err)
-	}
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	amt := accountID.BuyingPower.Div(decimal.NewFromInt(2))
+// 	openPrice := bars[0].Open
 
-	order := alpaca.PlaceOrderRequest{
-		AccountID: accountID.ID,
-		AssetKey: &symbol,
-		Notional: amt,
-		Side: "buy",
-		Type: "market",
-		TimeInForce: "day",
-	}
-	client.PlaceOrder(order)
-}
+// 	return openPrice
+// }
+
+// func buyOrder(symbol string, client *alpaca.Client) {
+// 	accountID, err := client.GetAccount()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	amt := accountID.BuyingPower.Div(decimal.NewFromInt(2))
+
+// 	order := alpaca.PlaceOrderRequest{
+// 		AccountID: accountID.ID,
+// 		AssetKey: &symbol,
+// 		Notional: amt,
+// 		Side: "buy",
+// 		Type: "market",
+// 		TimeInForce: "day",
+// 	}
+// 	client.PlaceOrder(order)
+// }
